cbd461b807368884c40c574c1e4ec152ebbe6796
Daniel Antonio <daniel23.da74@gmail.com>
2025-02-24 23:29:20 -0600

    fear(sitemap): obteniendo solo productos activos

--- app/Modules/Product/Repositories/ProductRepository.php
+++ app/Modules/Product/Repositories/ProductRepository.php
@@ -11,7 +11,7 @@ class ProductRepository
 
     public function getAllProduct(): Collection|array
     {
-        return $this->productModel->all();
-        //        return $this->productModel::where('is_active', 1)->get();
+        //        return $this->productModel->all();
+        return $this->productModel::where('estatus_crud', 'C')->get();
     }
 }
